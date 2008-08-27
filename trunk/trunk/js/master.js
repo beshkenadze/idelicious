a424766646e1b05e7e3d5aca6fb9f07d4e2bce32
beshkenadze <beshkenadze@32bc51cb-7655-0410-8c4b-49992bbd37a5>
2008-08-27 20:29:44 +0000

    init

---
+++ trunk/trunk/js/master.js
@@ -0,0 +1,35 @@
+google.load("jquery", "1.2.6");
+google.load("jqueryui", "1.5.2");
+window.onload = function(){
+    $("#getButton").click(function(){
+        $('#images').iDelicious({
+            user: $('#user').val(),
+            count: $('#count').val(),
+            col: $('#col').val()
+        });
+    });
+    $("#getMeCode").click(function(){
+        $("#code").val('<script src="http://www.google.com/jsapi"></script>\n\
+<script>\n\
+google.load("jquery", "1.2.6");\n\
+google.load("jqueryui", "1.5.2");\n\
+</script>\n\
+<script type="text/javascript" src="http://gg.agrr.org/js/iDelicious.js"></script>\n\
+<script>\n\
+google.load("jquery", "1.2.6");\n\
+google.load("jqueryui", "1.5.2");\n\
+window.onload = function(){\n\
+$(\'#place_for_image\').iDelicious({\n\
+    user: "'+$('#user').val()+'", //username\n\
+    count: '+$('#count').val()+', //count urls: max 15\n\
+    col: '+$('#col').val()+', //Cols\n\
+    pathToPreloadImage: \'/images/load.gif\', //Path to preload image\n\
+    startTop:150, //Start absolute position in top\n\
+    startLeft: 0 //Start absolute position in left\n\
+});\n\
+}\n\
+</script>\n\
+<div id="place_for_image"></div>\
+')
+    });
+}
